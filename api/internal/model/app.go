00c46c282511e5c7c087a6a9ad29234e47998a47
majid <majid0karimi@gmail.com>
2025-08-05 12:37:12 +0330

    add list app function

--- api/internal/model/app.go
+++ api/internal/model/app.go
@@ -10,3 +10,19 @@ type App struct {
 	Image   string `json:"image"`
 	Status  string `json:"status"`
 }
+
+type PodStatus struct {
+	Name     string `json:"name"`
+	Status   string `json:"status"`
+	Ready    string `json:"ready"`
+	Restarts string `json:"restarts"`
+	Age      string `json:"age"`
+}
+
+type AppStatus struct {
+	Name      string      `json:"name"`
+	Namespace string      `json:"namespace"`
+	Status    string      `json:"status"`
+	Age       string      `json:"age"`
+	Pods      []PodStatus `json:"pods"`
+}
